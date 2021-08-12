d811119ec0ade59e941d9257043894c4e44c79df
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-08-12 11:59:26 +0200

    Correction indentation et erreurs W3C

--- JS/services/ProductService.js
+++ JS/services/ProductService.js
@@ -2,23 +2,22 @@ import {Product} from "../models/Product.js";
 
 export class ProductService {
 
-    constructor() {}
+	constructor() {}
 
-    async getProduct(productId) {
-        //Récupération de l'id via l'API
-        return fetch(`http://localhost:3000/api/cameras/` + productId)
-            .then((data) => {
-                return data.json(); 
-            })
-            .then((rawData) => new Product(rawData._id, rawData.name, rawData.description, rawData.imageUrl, rawData.price, rawData.lenses))
-    }
+	async getProduct(productId) {
+		//Récupération de l'id via l'API
+		return fetch(`http://localhost:3000/api/cameras/` + productId)
+			.then((data) => {
+				return data.json();
+			})
+			.then((rawData) => new Product(rawData._id, rawData.name, rawData.description, rawData.imageUrl, rawData.price, rawData.lenses))
+	}
 
-
-    displayProduct(product) {
-        const containerProduct = document.getElementById('containerproduct');
-        containerProduct.insertAdjacentHTML(
-            "beforeend",
-            `
+	displayProduct(product) {
+		const containerProduct = document.getElementById('containerproduct');
+		containerProduct.insertAdjacentHTML(
+			"beforeend",
+			`
             <article class="article">
             <img class="article__image" src="${product.image}">
             <h2 class="article__title">${product.name}</h2>
@@ -34,48 +33,48 @@ export class ProductService {
             <a class="addCart" href="panier.html"><button id="btnCart" class="products__button" href="panier.html">Ajouter au panier</button></a>
         </div> 
         </article>`
-        );
-        
-        //Pour la sélection des objectifs
-        const lenses = product.lenses;
-        lenses.forEach(function (lens) {
-        const objectif = document.getElementById('objectif');
-        objectif.insertAdjacentHTML("beforeend", `<option>${lens}</option>`);
-        });
+		);
+
+		//Pour la sélection des objectifs
+		const lenses = product.lenses;
+		lenses.forEach(function(lens) {
+			const objectif = document.getElementById('objectif');
+			objectif.insertAdjacentHTML("beforeend", `<option>${lens}</option>`);
+		});
 
 
-        //On écoute le bouton ajouter au panier au click
-        let addToCartBtn = document.getElementById('btnCart');
-            addToCartBtn.addEventListener('click', (event) => {
-        
-        /*************************************************LOCAL STORAGE****************************************/
-        //Création d'un tableau contenant les infos d'un article
-        let productOptions = 
-            {
-                image: product.image,
-                name: product.name,
-                id: product.id,
-                price: product.price / 100,
-            };
-            
-        let localStorageProduct = JSON.parse(localStorage.getItem("product"));
+		//On écoute le bouton ajouter au panier au click
+		let addToCartBtn = document.getElementById('btnCart');
+		addToCartBtn.addEventListener('click', (event) => {
 
-        //Pour ajouter un produit séléctionné dans le localStorage
-        const addLocalStorage = () => {
-            //Ajout dans le tableau de l'objet avec les valeurs choisies par l'utilisateur
-            localStorageProduct.push(productOptions);
-            //Transformation en format JSON et envoyer dans la key article du localStorage
-            localStorage.setItem("product", JSON.stringify(localStorageProduct));
-        };
-        
-        //Si il y a déjà un produit dans le localStorage
-        if(localStorageProduct){
-            addLocalStorage();
-        //Si il n'y a pas de produit dans le localStorage
-        } else {
-            localStorageProduct = [];
-            addLocalStorage();
-        }
-        });
-    }
+			/*************************************************LOCAL STORAGE****************************************/
+			//Création d'un tableau contenant les infos d'un article
+			let productOptions = {
+				image: product.image,
+				name: product.name,
+				id: product.id,
+				price: product.price / 100,
+			};
+
+			let localStorageProduct = JSON.parse(localStorage.getItem("product"));
+
+
+			//Pour ajouter un produit séléctionné dans le localStorage
+			const addLocalStorage = () => {
+				//Ajout dans le tableau de l'objet avec les valeurs choisies par l'utilisateur
+				localStorageProduct.push(productOptions);
+				//Transformation en format JSON et envoyer dans la key article du localStorage
+				localStorage.setItem("product", JSON.stringify(localStorageProduct));
+			};
+
+			//Si il y a déjà un produit dans le localStorage
+			if (localStorageProduct) {
+				addLocalStorage();
+				//Si il n'y a pas de produit dans le localStorage
+			} else {
+				localStorageProduct = [];
+				addLocalStorage();
+			}
+		});
+	}
 }
